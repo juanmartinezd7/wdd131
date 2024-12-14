0359d794f7fc8968171cebf982e8596ed634718d
rysmartinez <rysmartinez7@gmail.com>
2024-12-14 01:40:34 -0600

    signup count

--- project/siteplan.js
+++ project/siteplan.js
@@ -20,14 +20,14 @@ document.addEventListener("DOMContentLoaded", function () {
   
     if (!incrementedFlag) {
       // Retrieve the current count from localStorage or initialize it to 0
-      let reviewCount = localStorage.getItem("signupCount");
-      reviewCount = reviewCount ? parseInt(reviewCount, 10) : 0;
+      let signupCount = localStorage.getItem("signupCount");
+      signupCount = signupCount ? parseInt(signupCount, 10) : 0;
   
       // Increment the counter
-      reviewCount += 1;
+      signupCount += 1;
   
       // Save the updated count back to localStorage
-      localStorage.setItem("signupCount", reviewCount);
+      localStorage.setItem("signupCount", signupCount);
   
       // Mark the counter as incremented in this session
       sessionStorage.setItem("counterIncremented", "true");
@@ -35,6 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
   
     // Display the current count on the page
     const counterDisplay = document.getElementById("signupCounter");
-    const reviewCount = localStorage.getItem("signupCount") || 0;
-    counterDisplay.textContent = `Signups Completed: ${reviewCount}`;
+    const signupCount = localStorage.getItem("signupCount") || 0;
+    counterDisplay.textContent = `Signups Completed: ${signupCount}`;
   });
